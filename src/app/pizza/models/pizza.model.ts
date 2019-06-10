858e65887aec0b5848fbe48735153eddda853c72
Artur <arturjataide@gmail.com>
2019-06-10 18:44:11 +0200

    Complete CRUD operations

--- src/app/pizza/models/pizza.model.ts
+++ src/app/pizza/models/pizza.model.ts
@@ -1,5 +1,12 @@
-export interface Pizza {
+export class Pizza {
     id: string;
     size: string;
     name: string;
+
+    constructor(id: string, size: string, name: string) {
+        this.id = id;
+        this.size = size;
+        this.name = name;
+    }
+
 }
--- src/app/pizza/services/pizza.service.ts
+++ src/app/pizza/services/pizza.service.ts
@@ -2,37 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 
 import { Pizza } from '../models/pizza.model';
-
+import {
+    EntityCollectionServiceBase,
+    EntityCollectionServiceElementsFactory,
+} from '@ngrx/data';
 
 @Injectable()
-export class PizzasService {
-  constructor(private http: HttpClient) {}
-  url = 'http://localhost:9090';
-
-  getPizzas(): Observable<Pizza[]> {
-    return this.http
-      .get<Pizza[]>(`${this.url}/pizzas`)
-      .pipe(catchError((error: any) => throwError(error.json())));
-  }
-
-  createPizza(payload: Pizza): Observable<Pizza> {
-    return this.http
-      .post<Pizza>(`${this.url}/pizzas`, payload)
-      .pipe(catchError((error: any) => throwError(error.json())));
-  }
-
-  updatePizza(payload: Pizza): Observable<Pizza> {
-    return this.http
-      .put<Pizza>(`${this.url}/pizzas/${payload.id}`, payload)
-      .pipe(catchError((error: any) => throwError(error.json())));
-  }
-
-  removePizza(payload: Pizza): Observable<Pizza> {
-    return this.http
-      .delete<any>(`/pizzas/${payload.id}`)
-      .pipe(catchError((error: any) => throwError(error.json())));
-  }
+export class PizzasService extends EntityCollectionServiceBase<Pizza> {
+    constructor(
+        elementsFactory: EntityCollectionServiceElementsFactory,
+        private http: HttpClient
+    ) {
+        super('Pizza', elementsFactory);
+    }
 }
